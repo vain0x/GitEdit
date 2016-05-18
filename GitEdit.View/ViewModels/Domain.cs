81009b636fff06a07afe013e0c1a93ef23ce8998
vain0 <id_vain@yahoo.co.jp>
2016-05-18 21:51:06 +0900

    Add syntax highlight definition for .gitignore

--- GitEdit.View/ViewModels/Domain.cs
+++ GitEdit.View/ViewModels/Domain.cs
@@ -12,12 +12,14 @@ namespace GitEdit.View
         public static string AppName = "GitEdit";
 
         public static string CommitMessageSyntaxName = "CommitMessage";
+        public static string GitIgnoreSyntaxName = "GitIgnore";
         public static string IniSyntaxName = "Ini";
 
         public static Tuple<string, string[]>[] SyntaxDefinitions =
             new[]
             {
                 Tuple.Create(CommitMessageSyntaxName, new string[] { }),
+                Tuple.Create(GitIgnoreSyntaxName, new string[] { ".gitignore" }),
                 Tuple.Create(IniSyntaxName, new string[] { ".ini", ".cfg", ".gitconfig" }),
             };
     }
